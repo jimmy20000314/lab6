9ed5d0f64c3de58f4eb6884718eb4b8311ac1c92
jimmy20000314 <145492034+jimmy20000314@users.noreply.github.com>
2023-12-14 20:41:37 +0800

    Add files via upload

---
+++ vvd_caravel_fpga.srcs/sources_1/new/sources_1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/12/14 11:27:50
+// Design Name: 
+// Module Name: sources_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sources_1(
+
+    );
+endmodule
